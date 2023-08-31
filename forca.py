e7216b679e6247d3b5b1b1b346b94868407202a1
Bruna Clares Cardoso <143137943+bruclares@users.noreply.github.com>
2023-08-31 16:57:00 -0300

    atualização do projeto

--- forca.py
+++ forca.py
@@ -4,6 +4,27 @@ def jogar():
     print("****Bem vindo ao jogo de Forca!****")
     print("***********************************")
 
+    palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_"]
+    letras_faltando = str(letras_acertadas.count("_"))
+    enforcou = False
+    acertou = False
+
+    print(letras_acertadas)
+    print("Ainda faltam acertar {} letras".format(letras_faltando))
+
+    # enquanto não enforcou e não acertou (enquanto true)
+    while (not enforcou and not acertou):
+        chute = input("Qual a letra? ")
+        chute = chute.strip() #retira os espaços da entrada
+        index = 0  # posição
+
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()): # função vai tornar as letras maisculas e comparar
+                letras_acertadas[index] = letra
+            index = index + 1     
+        print(letras_acertadas)
+
     print("Fim do jogo!")
 
 
